a033285758770ddba00ceb38103adb3dd6a156c3
slbateman <stevelbateman@gmail.com>
2021-10-06 12:58:26 -0600

    update

--- assets/main.js
+++ assets/main.js
@@ -1,6 +1,10 @@
-const gridContainer = document.getElementById("gridContainer")
+const gridContainer = document.getElementById("gridContainer");
+const grid = document.getElementById("grid");
 
 function uploadInspiration() {
-    gridContainer.style.backgroundImage = `url("https://picsum.photos/1000/700")`
+    gridContainer.style.backgroundImage = `url("https://picsum.photos/1000/700")`;
 }
+
+function gridGenerate(width, height) {
     
+}
