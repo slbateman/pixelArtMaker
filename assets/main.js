89c7a3e54d7db4f5b7e1d669360d3f66f63878c6
slbateman <stevelbateman@gmail.com>
2021-10-06 19:12:31 -0600

    end of day push

--- assets/main.js
+++ assets/main.js
@@ -1,25 +1,45 @@
-
-const insprationGen = document.getElementById("insprationGen");
 const gridContainer = document.getElementById("gridContainer");
 const grid = document.getElementById("grid");
-const widthBox = document.getElementById("width");
-const heightBox = document.getElementById("height");
 const resSubmit = document.getElementById("resSubmit");
 
-function uploadInspiration() {
+function inspirationRemove() {
+  gridContainer.style.backgroundImage = "none";
+}
+
+function inspirationGen() {
+  inspirationRemove();
   gridContainer.style.backgroundImage = `url("https://picsum.photos/1000/700")`;
 }
 
 // Make grid
-function gridGenerate(width, height) {
-  for (let i = 0; i < width; i++) {
+function gridGenerate() {
+  let widthBox = document.getElementById("width").value;
+  let heightBox = document.getElementById("height").value;
+  for (let i = 0; i < widthBox; i++) {
     const row = rowMake();
+    row.setAttribute("id",`row-${i}`);
     grid.appendChild(row);
-    for (let j = 0; j < height; j++) {
+    for (let j = 0; j < heightBox; j++) {
       const square = squareMake();
+      square.setAttribute("id",`row-${i}-${j}`);
       row.appendChild(square);
+      square.style.height = `${98 / heightBox}vh`
+      square.style.width = `${98 / heightBox}vh`
     }
   }
+}
+
+function gridRemove() {
+  const rowNum = document.querySelectorAll(".row");
+  for (let i = 0; i < rowNum.length; i++) {
+      let row = document.getElementById(`row-${i}`)
+    grid.removeChild(row);
+  }
+}
+
+function makeGrid() {
+    gridRemove();
+    gridGenerate();
 }
 
 // Make rows
@@ -37,5 +57,3 @@ function squareMake() {
 
   return square;
 }
-
-
