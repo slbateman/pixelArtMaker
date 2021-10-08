42a8b6d2e84ff7cc0aa62ebb11f46de40bab54fb
Steve Bateman <steve@steves-mac-mini.lan>
2021-10-08 13:08:08 -0600

    mid-day commit

--- assets/main.js
+++ assets/main.js
@@ -1,45 +1,23 @@
+
+// Global Variable Decloration
 const gridContainer = document.getElementById("gridContainer");
 const grid = document.getElementById("grid");
 const resSubmit = document.getElementById("resSubmit");
+const fillButton = document.getElementById("fill");
+const colorPalettesList = document.querySelectorAll(".colorSlot")
+const toolList = document.querySelectorAll(".tool")
+let randomNum = 1;
+let colorSlotActive = document.getElementById("colorSlot1")
+let paintColor = window.getComputedStyle(colorSlotActive).backgroundColor;
+let paintTool = document.getElementById("fill")
 
-function inspirationRemove() {
-  gridContainer.style.backgroundImage = "none";
-}
 
+// Generates random image. Uses the above variable to ensure
+// new image is generated after each click
 function inspirationGen() {
-  inspirationRemove();
-  gridContainer.style.backgroundImage = `url("https://picsum.photos/1000/700")`;
-}
-
-// Make grid
-function gridGenerate() {
-  let widthBox = document.getElementById("width").value;
-  let heightBox = document.getElementById("height").value;
-  for (let i = 0; i < widthBox; i++) {
-    const row = rowMake();
-    row.setAttribute("id",`row-${i}`);
-    grid.appendChild(row);
-    for (let j = 0; j < heightBox; j++) {
-      const square = squareMake();
-      square.setAttribute("id",`row-${i}-${j}`);
-      row.appendChild(square);
-      square.style.height = `${98 / heightBox}vh`
-      square.style.width = `${98 / heightBox}vh`
-    }
-  }
-}
-
-function gridRemove() {
-  const rowNum = document.querySelectorAll(".row");
-  for (let i = 0; i < rowNum.length; i++) {
-      let row = document.getElementById(`row-${i}`)
-    grid.removeChild(row);
-  }
-}
-
-function makeGrid() {
-    gridRemove();
-    gridGenerate();
+  gridContainer.style.backgroundImage =
+    "url(https://picsum.photos/1000/700?random=" + randomNum;
+  randomNum++;
 }
 
 // Make rows
@@ -57,3 +35,80 @@ function squareMake() {
 
   return square;
 }
+
+// Generates grid based on user input
+function gridGenerate() {
+  // Pulls data from input fields
+  let widthBox = document.getElementById("width").value;
+  let heightBox = document.getElementById("height").value;
+  // Generates rows
+  for (let i = 0; i < widthBox; i++) {
+    const row = rowMake();
+    // Assigns ID to row to enable removal later
+    row.setAttribute("id", `row-${i}`);
+    grid.appendChild(row);
+    // Generates squares
+    for (let j = 0; j < heightBox; j++) {
+      const square = squareMake();
+      // Assigns ID to square to enable color change later
+      square.setAttribute("id", `row-${i}-${j}`);
+      row.appendChild(square);
+      // Changes square height and width based on height of window
+      // and number of squares desired
+      square.style.height = `${98 / heightBox}vh`;
+      square.style.width = `${98 / heightBox}vh`;
+    }
+  }
+}
+
+// Removes any existing grid
+function gridRemove() {
+  const rowNum = document.querySelectorAll(".row");
+  for (let i = 0; i < rowNum.length; i++) {
+    let row = document.getElementById(`row-${i}`);
+    grid.removeChild(row);
+  }
+}
+
+// This function will wipe the grid before regenerating it
+// in case there is a grid already in place
+function makeGrid() {
+  gridRemove();
+  gridGenerate();
+}
+
+// Fill all squares with color
+function fillSquares() {
+  fillButton.addEventListener('click', () => {
+    const allSquares = document.querySelectorAll('.square');
+  allSquares.forEach(square => (square.style.backgroundColor = paintColor));
+});
+}
+
+// Color Palette Selector to assign active color
+function colorActive(){
+  for (let i = 0; i < colorPalettesList.length; i++)
+    colorPalettesList[i].addEventListener('click', () => {
+      colorSlotActive.classList.remove("active");
+      colorSlotActive = colorPalettesList[i];
+      colorSlotActive.classList.add("active");
+    });
+}
+
+// Tool Selector to assign active tool
+function toolActive(){
+  for (let i = 0; i < toolList.length; i++)
+    toolList[i].addEventListener('click', () => {
+      paintTool.classList.remove("active");
+      paintTool = toolList[i];
+      paintTool.classList.add("active");
+    });
+}
+
+function init(){
+  fillSquares();
+  colorActive();
+  toolActive();
+}
+
+init();
